bd856f9323cb6969b525d152accc0d43cf6a19b9
topinkaeu <topin3451@gmail.com>
2026-07-26 14:16:32 +0700

    Save local changes

--- lib/NetScope/models/observation.dart
+++ lib/NetScope/models/observation.dart
@@ -1,3 +1,16 @@
+enum Severity { info, warning, critical }
+
+class Observation {
+  final String title;
+  final String description;
+  final Severity severity;
+  Observation({
+    required this.title,
+    required this.description,
+    required this.severity,
+  });
+}
+
 class observation {
   final String title;
   final String description;
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
-import 'package:final_project/NetScope/services/packet_parser.dart';
+// import 'package:final_project/NetScope/services/packet_parser.dart';
+
+import 'NetScope/services/packet_parser.dart';
 
 void main() {
   final file = File('test_pcap_file.pcap');
